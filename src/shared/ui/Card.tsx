2a94349eaa1bca3b9ca90ad29b3520c829db701b
KyawZinLin <kyawzinlin@Qhrm.io>
2025-09-16 23:57:35 +0630

    set up folder structure

---
+++ src/shared/ui/Card.tsx
@@ -0,0 +1,6 @@
+export default function Card({children}:{children: React.ReactNode})
+{
+    <div className="rounded-xl border p-4 shadow=sm bg-white dark:bg-gray-800">
+        {children}
+    </div>
+}
